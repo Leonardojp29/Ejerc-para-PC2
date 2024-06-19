4efad246857d3968d3133fc3163d672ff3948bba
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-06-18 20:26:16 -0500

    Cambios

--- src/main/java/com/cibertec/cibertec/repository/ClienteRepository.java
+++ src/main/java/com/cibertec/cibertec/repository/ClienteRepository.java
@@ -1,11 +1,21 @@
 package com.cibertec.cibertec.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cibertec.cibertec.entity.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
     
-
+    //Aplicando Query methods
+    public abstract List<Cliente> findByDni(String dni);
+    public abstract List<Cliente> findByNombresLike(String nombres);
     
+    //Aplicadon JPQL (Java Persistence Query Language)
+    @Query("SELECT x FROM Cliente x WHERE x.dni = ?1 AND x.nombres = ?2")
+    public abstract List<Cliente> findByDniAndNombres(String dni, String nombres);
+    
+
 }
--- src/main/java/com/cibertec/cibertec/service/ClienteService.java
+++ src/main/java/com/cibertec/cibertec/service/ClienteService.java
@@ -1,12 +1,23 @@
 package com.cibertec.cibertec.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.cibertec.cibertec.entity.Cliente;
 
 public interface ClienteService {
     
-
+    //Para el CRUD
     public abstract List<Cliente> listaCliente();
+    public abstract Cliente insertaActualizaCliente(Cliente obj);
+    public abstract void eliminaCliente(int id);
+    public abstract Optional<Cliente> obtienePorId(int id);
+    
+    //Para consultas
+    public abstract List<Cliente> listaClientePorNombre(String nombres);
+    public abstract List<Cliente> listaClientePorDni(String dni);
+    public abstract List<Cliente> listaClientePorDniNombre(String dni, String nombres);
+
+   
 
 }
